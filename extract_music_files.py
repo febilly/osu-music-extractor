1ce78147ae1d4fead386e432cd7db05af442b5b7
febilly <fake@fake.com>
2024-10-09 03:43:52 +0800

    fully rewritten :D new README incoming *soon*™

--- extract_music_files.py
+++ extract_music_files.py
@@ -1,102 +1,273 @@
-def locate(d,outputdir):
-    import os
-
-    size_l = []
-    mp3 = []
-
-    for i in os.listdir(d):
-        i = d + "/" + i
-        if os.path.isdir(i):
-            continue
-        if i[len(i)-4:len(i)] != ".mp3" or os.path.getsize(i) < 307200:
-            continue
-        mp3.append(i)
-        size_l.append(os.path.getsize(i))
-    
-    if mp3 == []:
-        return(False)
-
-    return(mp3[size_l.index(max(size_l))])
-
-
-#import getpass
-import os
+import io
 import shutil
 import tkinter
 from tkinter import filedialog
+from pathlib import Path
+import zipfile
+import eyed3
+from tqdm import tqdm
+# from rich import print
+from dataclasses import dataclass
+import logging
 
-title_artist = True
+PUT_TITLE_BEFORE_ARTIST = True
 
-#default_output_directory = "C:/Users/{}/AppData/Local/osu!/Songs".format(getpass.getuser())
-#default_output_directory = os.path.expanduser("~/AppData/Local/osu!/Songs")
+invalid_chars = ['<', '>', ':', '"', '/', '\\', '|', '?', '*']
+substitute_char = '-'
 
-root = tkinter.Tk()
-root.withdraw()
+songs_counter = 0
 
-inputdir = filedialog.askdirectory(title = "Select the \"Songs\" Folder.",initialdir = os.path.expanduser("~/AppData/Local/osu!/Songs"))
-if inputdir == "":
-    exit()
+logging.getLogger().setLevel(logging.ERROR)
 
-#inputdir = "C:/Users/{}/AppData/Local/osu!/Songs".format(getpass.getuser())
-outputdir = filedialog.askdirectory(title = "Select a output Folder.")
-if outputdir == "":
-    exit()
+@dataclass
+class Song:
+    path: Path
+    title: str
+    artist: str
+    filename: str
 
-print("Inputdir:  " + inputdir)
-print("Outputdir: " + outputdir)
-print("Press Enter to start.")
-input()
-
-in_l = []
-out_l = []
-
-for song in os.listdir(inputdir):
-    songdir = inputdir + "/" + song
-    print("Scanning " + songdir)
-
-    if os.path.isfile(songdir):
-        print(songdir + " isn't a directory. Skipped.\n")
-        continue
-
-    found = locate(songdir,outputdir)
-    if found == False:
-        print("Nothing found in " + songdir + ". Skipped.\n")
-        continue
+def get_section(osu_path: Path, section: str) -> dict[str, str]:
+    """
+    Get a section from a .osu file.
+    """
+    with osu_path.open('r', encoding='utf-8') as f:
+        lines = f.readlines()
+    lines = [line for line in lines if line.strip()]
     
-    name = song
+    metadata: dict[str, str] = {}
+    in_section = False
+    for line in lines:
+        if line.startswith(section):
+            in_section = True
+            continue
+        if in_section:
+            if line.startswith('['):
+                break
+            key, value = line.split(':', 1)
+            metadata[key.strip()] = value.strip()
+
+    return metadata
+
+def sanitize_filename(filename: str) -> str:
+    """
+    Sanitize a filename by replacing invalid characters with a substitute character.
+    """
+    for char in invalid_chars:
+        filename = filename.replace(char, substitute_char)
+    
+    return filename
+
+def analyse_folder(folder: Path) -> Song | None:
+    """
+    Analyse a beatmap folder and return a Song object if the folder contains a valid song.
+    """
+    file_paths: list[Path] = []
+    for file in folder.iterdir():
+        if file.is_file():
+            file_paths.append(file)
+            
+    osu_file = None
+    for file in file_paths:
+        if file.suffix == '.osu':
+            osu_file = file
+            break
+    
+    if not osu_file:
+        return None
+    
+    general = get_section(osu_file, '[General]')
+    metadata = get_section(osu_file, '[Metadata]')
+    
+    audiofile = folder / general['AudioFilename']
+    if not audiofile.exists():
+        return None
+    
+    if PUT_TITLE_BEFORE_ARTIST:
+        filename = f'{metadata['Title']} - {metadata['Artist']}.mp3'
+    else:
+        filename = f'{metadata['Artist']} - {metadata['Title']}.mp3'
+    filename = sanitize_filename(filename)
+    
+    song = Song(
+        path=audiofile,
+        title=metadata['TitleUnicode'] if 'TitleUnicode' in metadata else metadata['Title'],
+        artist=metadata['ArtistUnicode'] if 'ArtistUnicode' in metadata else metadata['Artist'],
+        filename=filename
+    )
+    
+    return song
+
+def has_metadata(file: Path) -> bool:
+    """
+    Check if a mp3 file has metadata.
+    assumes that the file is a valid mp3 file.
+    """
+    audiofile = eyed3.load(file)
+    assert audiofile is not None
+    tag = audiofile.tag
+    
+    return tag is not None and tag.artist and tag.title
+
+def is_valid_mp3(file: Path) -> bool:
+    """
+    Check if a file is a valid mp3 file.
+    """
     try:
-        if ord(song[:1]) in range(48,58):
-            name = name.split(" ",1)[1]
-        if title_artist:
-            name = name.split(" - ",1)[1] + " - " + name.split(" - ",1)[0]
-            #name = name[name.index(" - ") + 3:] + " - " + name[:name.index(" - ")]
-        
-        name += ".mp3"
-        print(song)
-        print(name)
-    except BaseException:
-        print("Error when processing the name. Skipped.\n")
-        continue
+        audiofile = eyed3.load(file)
+        return audiofile is not None and audiofile.info is not None
+    except Exception:
+        return False
+
+def write_song(song: Song, outputdir: Path):
+    """
+    Write a song to the output directory.
+    it checks the validity of the mp3 file and writes the metadata.
+    it also renames ogg files back to mp3.
+    """
+    global songs_counter
     
-    outputpath = outputdir + "/" + name
-    if os.path.exists(outputpath):
-        print(outputpath + " already exists. Skipped.\n")
-        continue
+    outputpath = outputdir / song.filename
+    
+    if outputpath.exists():
+        return
+    
+    # shutil.copy(song.path, outputpath)
+    outputpath.write_bytes(song.path.read_bytes())
+    songs_counter += 1
+    
+    if not is_valid_mp3(outputpath):  # some 'mp3' files are actually other formats
+        with open(outputpath, 'rb') as f:
+            header = f.read(4)
+            
+        if header == b'OggS':  # the most common actual format
+            new_outputpath = outputpath.with_suffix('.ogg')
+            shutil.move(outputpath, new_outputpath)
+            print(f"\nRenamed {outputpath.name} to {new_outputpath.name} since it's actually an ogg file.")
+        else:
+            print(f"\nWarning: {outputpath.name} is not a valid mp3 file.")
+        
+        return
+        
+    # if has_metadata(outputpath):
+    #     return
+    
+    audiofile = eyed3.load(outputpath)
+    assert audiofile is not None
+    if audiofile.tag is None:  # make sure the mp3 file have a tag
+        audiofile.initTag()
+    
+    # write metadata
+    tag = audiofile.tag
+    assert tag is not None
+    tag.artist = song.artist
+    tag.title = song.title
+    
+    try:  # eyed3 don't support ID3 v2.2 so we just ignore it
+        tag.save(encoding='utf-8')
+    except NotImplementedError:
+        pass
 
-    print("\n")
+def process_beatmap_folder(folder: Path, outputdir: Path):
+    """
+    Analyse the folder and write the song if it contains one.
+    """
+    result = analyse_folder(folder)
+    if result:
+        write_song(result, outputdir)
 
-    in_l.append(found)
-    out_l.append(outputpath)
+def process_catalog_directory(directory: Path, outputdir: Path):
+    """
+    process the directory that contains the beatmap folders.
+    """
+    paths_to_scan = []
+    
+    for path in directory.iterdir():
+        if path.is_dir():
+            paths_to_scan.append(path)
+        elif path.suffix == '.osz':
+            paths_to_scan.append(zipfile.Path(path))
+            
+    print("Processing individual beatmaps...")
+    for path in tqdm(paths_to_scan):
+        process_beatmap_folder(path, outputdir)
 
-if in_l == []:
-    print("Nothing found in " + inputdir + ".\nPress Enter to exit.")
+def process_pack(pack: Path, outputdir: Path):
+    """
+    Process a .zip file that contains serveral .osz files.
+    """
+    compressed_osz_paths: list[zipfile.Path] = []
+    
+    zipfile_path = zipfile.Path(pack)
+    for path in zipfile_path.iterdir():
+        if path.suffix == '.osz':
+            compressed_osz_paths.append(path)
+            
+    for compressed_osz_path in compressed_osz_paths:
+        osz_data = compressed_osz_path.read_bytes()
+        osz_bytes = io.BytesIO(osz_data)
+        inner_path = zipfile.Path(osz_bytes)
+        
+        process_beatmap_folder(inner_path, outputdir)
+            
+        del osz_bytes
+        del osz_data
+
+def process_everything(inputdir: Path, outputdir: Path):
+    """
+    Process everything (beatmap folders, .osz files, and beatmap packs) in inputdir.
+    """
+    process_catalog_directory(inputdir, outputdir)
+    
+    potential_packs = []
+    for path in inputdir.iterdir():
+        if path.suffix == '.zip':
+            potential_packs.append(path)
+
+    print("Processing beatmap packs...")
+    for pack in tqdm(potential_packs):
+        process_pack(pack, outputdir)
+
+def ask_quit():
+    print("Press Enter to exit.")
     input()
     exit()
 
-for i in range(len(in_l)):
-    print("From: " + in_l[i])
-    print("To:   " + out_l[i])
-    shutil.copy(in_l[i],out_l[i])
-    print("Copied.\n")
+def ask_for_directories() -> tuple[Path, Path]:
+    """
+    Ask the user for the input and output directories.
+    """
+    root = tkinter.Tk()
+    root.withdraw()
 
-print("Done.")
+    inputdir = filedialog.askdirectory(
+        title="Select the \"Songs\" Folder.", initialdir=str(Path.home() / "AppData/Local/osu!/Songs"))
+    if not inputdir:
+        exit()
+
+    outputdir = filedialog.askdirectory(title="Select a output Folder.")
+    if not outputdir:
+        exit()
+
+    return Path(inputdir), Path(outputdir)
+
+def main():
+    global songs_counter
+    
+    inputdir, outputdir = ask_for_directories()
+
+    if inputdir == outputdir:
+        print("Input and output directory can't be the same.")
+        ask_quit()
+
+    print(f"Inputdir:  {inputdir}")
+    print(f"Outputdir: {outputdir}")
+    print("Press Enter to start, or close this window to cancel.")
+    input()
+
+    process_everything(inputdir, outputdir)
+
+    print(f"\nDone. {songs_counter} songs extracted.")
+    ask_quit()
+
+if __name__ == "__main__":
+    main()
